d6d9828517128249866361c746fcbc53d9f5e380
pawantiwari9575 <pavantiwari9575@gmail.com>
2021-03-10 17:29:31 +0530

    pawan

---
+++ script.js
@@ -0,0 +1,50 @@
+gsap.to('#videoplyr',{
+    scrollTrigger:{
+        trigger:"#main",
+        start: "top top",
+        scrub:2,
+        pin:"#main"
+    },
+    width:"100%",
+})
+
+gsap.to('#one',{
+    scrollTrigger:{
+        trigger:"#main",
+        start: "top top",
+        scrub:3,
+    },
+    left:"-400px",
+    opacity:0
+})
+
+gsap.to('#holdr1',{
+    scrollTrigger:{
+        trigger:"#main",
+        start: "top top",
+        scrub:3,
+    },
+    left:"-400px",
+    opacity:0
+})
+
+
+gsap.to('#sx',{
+    scrollTrigger:{
+        trigger:"#main",
+        start: "top top",
+        scrub:3,
+    },
+    right:"-400px",
+    opacity:0
+})
+
+gsap.to('#holdr2',{
+    scrollTrigger:{
+        trigger:"#main",
+        start: "top top",
+        scrub:3,
+    },
+    right:"-400px",
+    opacity:0
+})
